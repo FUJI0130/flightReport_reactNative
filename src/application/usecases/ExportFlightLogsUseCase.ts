f10ea538cfcd694a9d5b2b4827827aeb3302c64b
FUJI0130 <sfujita19850130@gmail,com>
2024-08-04 12:41:34 +0900

    ExportScreenの削除

--- src/application/usecases/ExportFlightLogsUseCase.ts
+++
@@ -1,9 +0,0 @@
-// src/application/usecases/ExportFlightLogsUseCase.ts
-import {createFlightLogRepository} from '../../infrastructure/repositories/FlightLogRepositoryFactory';
-
-export class ExportFlightLogsUseCase {
-  async execute(): Promise<void> {
-    const flightLogRepository = await createFlightLogRepository();
-    await flightLogRepository.export();
-  }
-}
--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -4,7 +4,6 @@ import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../presentation/screens/HomeScreen';
 import AddRecordScreen from '../presentation/screens/AddRecordScreen';
 import DetailScreen from '../presentation/screens/DetailScreen';
-import ExportScreen from '../presentation/screens/ExportScreen';
 import FlightRecordsScreen from '../presentation/screens/FlightRecordsScreen';
 import NewFlightLogScreen from '../presentation/screens/NewFlightLogScreen'; // 新規作成画面を追加
 
@@ -27,7 +26,6 @@ const AppNavigator = () => {
         <Stack.Screen name="FlightRecords" component={FlightRecordsScreen} />
         <Stack.Screen name="AddRecord" component={AddRecordScreen} />
         <Stack.Screen name="Detail" component={DetailScreen} />
-        <Stack.Screen name="Export" component={ExportScreen} />
         <Stack.Screen name="NewFlightLog" component={NewFlightLogScreen} />
       </Stack.Navigator>
     </NavigationContainer>
